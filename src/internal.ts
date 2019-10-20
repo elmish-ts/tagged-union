7dc0599c822e1ef96beb6e1312b2f3826a770900
Derrick Beining <derrick.beining@gmail.com>
2019-10-20 14:28:42 -0400

    docs: hide internal values and declarations

--- src/internal.ts
+++ src/internal.ts
@@ -1,8 +1,11 @@
+/** @ignore */
 export const TAG: unique symbol = '@elmish-ts/tagged-union' as any
+/** @ignore */
 export type Unit = {
   readonly [TAG]: 'Unit'
   readonly Unit: {}
 }
+/** @ignore */
 export const Unit: Unit = {
   [TAG]: 'Unit',
   Unit: {}
